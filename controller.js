e4dbbf683395edf7c92bb4471fa437a2c9363543
wishmeluck@oansnb <oansmaturapax123@gmail.com>
2022-10-07 18:49:48 +0700

    succes tampilkan data

--- controller.js
+++ controller.js
@@ -6,3 +6,14 @@ var connection = require('./koneksi');
 exports.index = function(req,res){
     response.ok("Application Connected",res)
 };
+
+// Menampilkan semua data mahasiswa
+exports.tampilsemuamahasiswa = function(req,res){
+    connection.query('SELECT * FROM mahasiswa', function(error, rows, fields){
+        if(error){
+            connection.log(error);
+        }else{
+            response.ok(rows, res)
+        }
+    });
+};
